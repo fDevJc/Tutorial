f55a6970223bef29c7986b8de09f4860d072a1f0
fDevJc <fDevJc@gmail.com>
2021-02-23 23:36:57 +0900

    algorithm 12865 and modify .gitignore

--- python/python_algorithm/algorithm_12865_dp_2.py
+++ python/python_algorithm/algorithm_12865_dp_2.py
@@ -1,5 +1,10 @@
 n,k = map(int,input().split(" "))
-dp = [[0] * (n+10) for _ in range(k+10)]
+dp = [[0] * (n+2) for _ in range(k+2)]
+
+for i in range(len(dp)):
+	print()
+	for j in range(len(dp[i])):
+		print(dp[i][j], end="    ")
 
 arr = [0]
 
@@ -7,7 +12,9 @@ for i in range(n):
     arr.append(tuple(int(x) for x in input().split(" ")))
 
 for i in range(1,n+1):
+				print(i)
     for j in range(1,k+1):
+						print(j)
         if j >= arr[i][0]:
             dp[i][j]=max(dp[i - 1][j], (dp[i - 1][j - arr[i][0]]) + arr[i][1])
         else:
